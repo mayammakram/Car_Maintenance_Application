84387a38df4ca5bb54b1647a811179623888fbfa
OmarMiniesy <omarminiesy2003@gmail.com>
2022-12-03 14:58:46 +0200

    client class

--- finalMilestoneCode/client.h
+++ finalMilestoneCode/client.h
@@ -1,16 +1,20 @@
 #ifndef CLIENT_H
 #define CLIENT_H
-
+#include "Car_Portfolio.h"
+#include "Car_Portfolio.cpp"  
 #pragma once
 
 class client
 {
-public:
-    client();
-    ~client();
-
 private:
-
+    Car_Portfolio carPortfolio;
+    string address;
+    //bookingsAgenda bookings;
+public:
+    client(string, Car_Portfolio);
+    Car_Portfolio getCarPortfolio();
+    void setCarPortfolio(Car_Portfolio);
+    void viewPortfolio();
 };
 
 #endif
